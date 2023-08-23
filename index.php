6946c75dbd8204ccdb4a1dca25adfba94a088640
nekkox <108139374+nekkox@users.noreply.github.com>
2023-08-23 13:42:16 +0200

    Create Database class in Database.php file and add some more tweaks

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
 require "functions.php";
 require "router.php";
-
-
-class Database{
-
-    public function query(){
-
-        // Connect to the MySQL database.
-        $dsn = "mysql:host=localhost;port=3306;dbname=app;user=root;charset=utf8mb4";
-
-        // Tip: This should be wrapped in a try-catch.
-        $pdo = new PDO($dsn);
-
-        $statement = $pdo->prepare("select * from posts");
-        $statement->execute();
-
-        $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-        return $posts;
-    }
-}
+require "Database.php";
 
 $db = new Database();
-dd($db->query());
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
