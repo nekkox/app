ef4eeb89803603bbdcad98b3a4cd4452704eb866
nekkox <108139374+nekkox@users.noreply.github.com>
2023-10-03 23:14:00 +0200

    Tweak TextArea on notes/create view

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -7,12 +7,12 @@ $config = require base_path('config.php');
 $db = new Database($config['database']);
 $errors = [];
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-   if(! Validator::string($_POST['body'],1,100)){
-       $errors['body'] = 'A body of no more than 100 characters is required.';
-
-   }
+    if (!Validator::string($_POST['body'], 1, 100)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
+    }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
@@ -22,5 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-
-view("notes/create.view.php", ['heading' => "Create a note", 'errors' => $errors]);
+view("notes/create.view.php", [
+    'heading' => 'Create Note',
+    'errors' => $errors
+]);
