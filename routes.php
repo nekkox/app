3865671d78cc2d3289ed1fdf35c071fd6386ce35
nekkox <108139374+nekkox@users.noreply.github.com>
2023-10-07 21:24:05 +0200

    Create store controller for storing new user account in database. Create registration/create.view.php with register form

--- routes.php
+++ routes.php
@@ -10,5 +10,8 @@ $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
-$router->get('/note/edit', 'controllers/notes/edit.php');
-$router->patch('/note', 'controllers/notes/update.php');
+$router->get('/note/edit', 'controllers/notes/edit.php'); //Get the site /note/edit where you can edit the chosen note
+$router->patch('/note', 'controllers/notes/update.php'); //Update the note with patch method
+
+$router->get('/register', 'controllers/registration/create.php'); //Get the site /register where you can create new user
+$router->post('/register', 'controllers/registration/store.php'); //Store user in database if email does not exist
